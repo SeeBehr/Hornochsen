6aaab6b6110befb2d8f357511b473b26139773ea
Patrick <patrick_denz@hotmail.com>
2023-10-13 12:01:57 +0200

    Mein Zeug fuer die TextUI.

--- hornochsen/src/main/scala/TUILayout.scala
+++ hornochsen/src/main/scala/TUILayout.scala
@@ -1,22 +1,24 @@
-class Spieler(val name: String, val karten: Array[Int], val ochsen: Int) {
-        override def toString(): String = {
+class Spieler(val name: String, val karten: Vector[Int], val ochsen: Int) {
+    override def toString(): String = {
         name + ":\n\t" + karten.mkString(", ") + "\n\tOchsen: " + ochsen.toString() + "\n"
     }
 }
-class Reihe(val nummer: Int, val karten: Array[Int]) {
+
+class Reihe(val nummer: Int, val karten: Vector[Int]) {
     override def toString(): String = {
         "Reihe " + nummer + ": " + karten.mkString(", ")
     }
 }
-class Spielfeld(val reihen: Array[Reihe]){
-    override def toString(): String = {
+
+class Spielfeld(val reihen: Vector[Reihe]){
+    override def toString(): String = { 
         "Spielfeld:\n\t" + reihen.mkString("\n\t") + "\n"
     }
 }
 
 @main
 def start() =  {
-    val p = new Spieler("Patrick", Array(2, 7, 4), 0)
-    val f = new Spielfeld(Array(Reihe(1, Array(1, 2, 3)), Reihe(2, Array(1, 2, 3)), Reihe(3, Array(1, 2, 3))))
+    val p = new Spieler("Patrick", Vector(2, 7, 4), 0)
+    val f = new Spielfeld(Vector(Reihe(1, Vector(1, 2, 3)), Reihe(2, Vector(1, 2, 3)), Reihe(3, Vector(1, 2, 3))))
     println(p.toString() + "\n" + f.toString())
 }
