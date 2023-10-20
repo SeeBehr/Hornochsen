ce24a9d18ee366ed5b585483aa8ba23dacdcdcff
SeeBehr <sebi.baer@gmx.de>
2023-10-20 06:21:27 +0200

    Add commentary

--- hornochsen/src/main/scala/TUILayout.scala
+++ hornochsen/src/main/scala/TUILayout.scala
@@ -1,25 +1,31 @@
 
+// add case in front so you don't have to write new
 case class Spieler(val name: String, val karten: Vector[Int], val ochsen: Int) {
     override def toString(): String = {
         name + ":\n\tKarten: " + karten.mkString(", ") + "\n\tOchsen: " + ochsen.toString() + "\n"
     }
 }
 
+// add case in front so you don't have to write new
 case class Reihe(val nummer: Int, val karten: Vector[Int]) {
     override def toString(): String = {
         "Reihe " + nummer + ": " + karten.mkString(", ")
     }
 }
 
+// add case in front so you don't have to write new
 case class Spielfeld(val reihen: Vector[Reihe]){
     override def toString(): String = { 
         "Spielfeld:\n\t" + reihen.mkString("\n\t") + "\n"
     }
 }
-// Das ist ein collision command
+
+// Start of the Programm.
 @main
 def start() =  {
+    // create Spieler p as value
     val p = new Spieler("Patrick", Vector(2), 0)
-    val f = new Spielfeld(Vector(Reihe(1, Vector(7))))
-    println(p.toString() + "\n" + f.toString())
+    // create Spielfeld s as value
+    val s = new Spielfeld(Vector(Reihe(1, Vector(7))))
+    println(p.toString() + "\n" + s.toString())
 }
