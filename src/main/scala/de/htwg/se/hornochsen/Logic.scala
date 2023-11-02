a798483babfa0dfb7eb4fc83fc68895c5a5698de
SeeBehr <sebi.baer@gmx.de>
2023-11-02 15:35:47 +0100

    unlimited LOGIC

--- src/main/scala/de/htwg/se/hornochsen/Logic.scala
+++ src/main/scala/de/htwg/se/hornochsen/Logic.scala
@@ -1,14 +1,17 @@
-
 package de.htwg.se.hornochsen
 
 def which(cards: Vector[(Int, Player)]): ((Int, Player), Vector[(Int, Player)]) = {
-    var min = cards.min((x,y)=>x._1-y._1)
+    var min = cards.min((x,y)=>y._1-x._1)
     return (min, cards.filter(x => x._1 != min._1))
 }
 
 
 def where(b: Board, card: Int): Int = {
-    val lastElements: Vector[Int] = b.rows.map((row: Row) => row.cards(row.filled))
+    val lastElements: Vector[Int] = b.rows.map(row => row.cards(row.filled))
     val minRow = lastElements.min((x,y) => y-x)
     return lastElements.indexOf(minRow)
+}
+
+def canAdd(b: Board, index: Int): Boolean = {
+    return b.rows(index).filled < 6
 }
