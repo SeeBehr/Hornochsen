eac90800e244c3138324dd8ba306cb27bcd62d4a
SeeBehr <sebi.baer@gmx.de>
2023-11-17 12:12:45 +0100

    repaired tests

--- src/test/scala/de/htwg/se/hornochsen/controler/UpdateSpec.scala
+++ src/test/scala/de/htwg/se/hornochsen/controler/UpdateSpec.scala
@@ -20,11 +20,11 @@ class UpdateSpec extends AnyWordSpec {
             +", (2,Patrick:\n\tcards: 2\n\tOchsen: 0\n))")
         }
         "return an updated gamestate" in {
-            controler1.updateGamestate(WhichRowTake=(s:String, f:()=>String)=>1)._1.toString() should be ("Vector(Patrick:\n\tcards: \n\tOchsen: 0\n)") 
-            controler1.updateGamestate(WhichRowTake=(s:String, f:()=>String)=>1)._2.toString() should be ("Board:\n\tRow 1: 1, 2 filled: 2\n\nPlayed cards: \n")
+            controler1.updateGamestate(() => "1", WhichRowTake=(s:String, f:()=>String)=>1)._1.toString() should be ("Vector(Patrick:\n\tcards: \n\tOchsen: 0\n)") 
+            controler1.updateGamestate(() => "1", WhichRowTake=(s:String, f:()=>String)=>1)._2.toString() should be ("Board:\n\tRow 1: 1, 2 filled: 2\n\nPlayed cards: \n")
 
-            controler2.updateGamestate(WhichRowTake=(s:String, f:()=>String)=>1)._1.toString() should be ("Vector(Patrick:\n\tcards: \n\tOchsen: 1\n)") 
-            controler2.updateGamestate(WhichRowTake=(s:String, f:()=>String)=>1)._2.toString() should be ("Board:\n\tRow 1: 0, 0 filled: 1\n\nPlayed cards: \n0, von Patrick\n")
+            controler2.updateGamestate(() => "1", WhichRowTake=(s:String, f:()=>String)=>1)._1.toString() should be ("Vector(Patrick:\n\tcards: \n\tOchsen: 1\n)") 
+            controler2.updateGamestate(() => "1", WhichRowTake=(s:String, f:()=>String)=>1)._2.toString() should be ("Board:\n\tRow 1: 0, 0 filled: 1\n\nPlayed cards: \n0, von Patrick\n")
         }
     }
 }
