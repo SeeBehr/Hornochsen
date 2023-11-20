afdbd5e5ed48a355cc3157f38ba19a2ee54932eb
SeeBehr <sebi.baer@gmx.de>
2023-11-20 14:55:08 +0100

    added FasadeSpec

--- src/main/scala/de/htwg/se/hornochsen/util/Fasade.scala
+++ src/main/scala/de/htwg/se/hornochsen/util/Fasade.scala
@@ -5,10 +5,15 @@ import de.htwg.se.hornochsen.controler._
 import de.htwg.se.hornochsen.aview._
 
 object Fasade {
-    def initialiseGame(sizeDeck: Int = 120, numRows: Int = 4, numRowCards: Int = 6, numPlayer: Int = 4, numHandCards: Int = 6, input: Int => String = TUIplayerNames): GameState = {
-        val deck = initDeck(sizeDeck).shuffle()
+    def initialiseGame(shuffle: Boolean = true, sizeDeck: Int = 120, numRows: Int = 4, numRowCards: Int = 6, numPlayer: Int = 4, numHandCards: Int = 6, input: Int => String = TUIplayerNames): GameState = {
+        
+        val deck = if (shuffle) {
+            initDeck(sizeDeck).shuffle()
+        } else {
+            initDeck(sizeDeck)
+        }
         val (board, playerdeck) = initBoard(numRows, numRowCards, deck)
-        val (allP, refilldeck) = initAllPlayers(numPlayer, numHandCards, input, deck)
+        val (allP, refilldeck) = initAllPlayers(numPlayer, numHandCards, input, playerdeck)
         GameState(allP, board, refilldeck)
     }
 }
--- src/test/scala/de/htwg/se/hornochsen/util/FasadeSpec.scala
+++ src/test/scala/de/htwg/se/hornochsen/util/FasadeSpec.scala
@@ -6,7 +6,9 @@ import org.scalatest.matchers.should.Matchers._
 class FasadeSpec extends AnyWordSpec {
     "Fasade" should {
         "return a Gamestate" in {
-            Fasade.initialiseGame(sizeDeck=2,numRows=1, numRowCards=1, numPlayer=1, numHandCards=1,input = Int => "A")._1.toString() should be ("GameState(Vector(A:\n\tcards: 2\n\tOchsen: 0\n),Board:\n\tRow 1: 2 filled: 1\n\nPlayed cards: \n,Deck: 1\n)")
+            Fasade.initialiseGame(shuffle=false, sizeDeck=2, numRows=1, numRowCards=1, numPlayer=1, numHandCards=1, input = Int => "A")._1.toString() should be ("Vector(A:\n\tcards: 2\n\tOchsen: 0\n)")
+            Fasade.initialiseGame(shuffle=false, sizeDeck=2, numRows=1, numRowCards=1, numPlayer=1, numHandCards=1, input = Int => "A")._2.toString() should be ("Board:\n\tRow 1: 1 filled: 1\n\nPlayed cards: \n")
+            Fasade.initialiseGame(shuffle=false, sizeDeck=2, numRows=1, numRowCards=1, numPlayer=1, numHandCards=1, input = Int => "A")._3.toString() should be ("Deck: \n")
         }
     }
 }
