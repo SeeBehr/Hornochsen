e7bf04db3951e79eda4b2e5eab5d447828636004
SeeBehr <sebi.baer@gmx.de>
2023-12-08 10:40:50 +0100

    ..

--- src/newmain/scala/de/htwg/se/hornochsen/controler/Controler.scala
+++ src/newmain/scala/de/htwg/se/hornochsen/controler/Controler.scala
@@ -42,7 +42,66 @@ class Controler(var gamestate: GameState) extends Observable {
                 playersWaiting = gamestate.playersWaiting.tail,
                 board = gamestate.board.copy(playedCards=gamestate.board.playedCards.append((card, p)))
                 )
-                controler.notifyObservers(Event.nextPlayer)
+                if gamestate.playersWaiting.isEmpty then
+                    placeCards()
+                 else 
+                    controler.notifyObservers(Event.nextPlayer)
+            true
+        case false =>
+            false
+    }
+
+    def placeCards(): Unit = {
+        gamestate = gamestate.copy(
+            playersWaiting = gamestate.playersActive.appended(gamestate.playersDone),
+            playersDone = Vector.empty,
+            board = gamestate.board.copy(playedCards=Vector.empty)
+        )
+        val sorted = gamestate.board.playedCards.sortBy((card: Int, player: Player) => card: Int)
+        for (card, player) <- sorted do
+            gameState = gameState.copy(
+                playersActive = gameState.playersActive.updated(gameState.playersActive = gameState.playersWaiting.indexOf(player))
+                playersWaiting = gameState.playersWaiting.updated(gameState.playersWaiting.filterNot(_ == player))
+            )
+            val index: Int = where(gamestate.board, card)
+            index match 
+            case -1 => 
+                notifyObservers(Event.TakeRow)
+            case _ =>
+                if (canAdd(index)) then
+                    addCard(card, index)
+                else
+                    takeRow(player, index)
+    }
+
+    def where(b: Board, card: Int): Int = {
+        val lastElements: Vector[Int] = b.rows.map(row => row.cards(row.filled-1))
+        val possibleRows = lastElements.filter(x => (card - x) > 0)
+        if possibleRows.length == 0
+        then
+            return -1
+        return (lastElements.indexOf(possibleRows.sortBy(x=>(card-x)).head))
+    }
+
+    def canAdd(index: Int): Boolean = {
+        if index < 0 | index >= b.rows(0).cards.length then return false
+        return b.rows(index).filled < b.rows(index).cards.length
+    }
+
+    def addCard(card: Int, index: Int): Unit = {
+        gamestate = gamestate.copy(
+            board = gamestate.board.addCard(card, index+1)
+        )
+        notifyObservers(Event.CardsSelected)
+    }
+
+    def takeRow(player: Player, row: Int): Boolean = {
+        val canTake = player.canTake(row)
+        canTake match
+        case true =>
+            p.takeRow(row)
+            gamestate = gamestate.copy(
+                )
             true
         case false =>
             false
